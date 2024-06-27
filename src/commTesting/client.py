a22223a8fb0789a96761a1ae8ce945eeb9cef29d
prolvalone <christopher02michael@gmail.com>
2024-06-27 14:33:41 -0400

    video now runs on UDP connection

--- src/commTesting/client.py
+++ src/commTesting/client.py
@@ -1,42 +1,40 @@
-import struct
-import cv2
-import socket
-import pickle
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-hostIp = '172.16.224.229'  # Change to server's IP
-port = 7777
-serverAddress = (hostIp, port)
+# This is client code to receive video frames over UDP
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
 
-# Send initial message to server to initiate communication
-clientSocket.sendto(b'Hello', serverAddress)
+BUFF_SIZE = 65536
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+host_name = socket.gethostname()
+host_ip = '172.16.224.229'#  socket.gethostbyname(host_name)
+print(host_ip)
+port = 9999
+message = b'Hello'
 
-data = b""
-payloadSize = struct.calcsize("Q")
-
+client_socket.sendto(message,(host_ip,port))
+fps,st,frames_to_count,cnt = (0,0,20,0)
 while True:
-    while len(data) < payloadSize:
-        packet, _ = clientSocket.recvfrom(4 * 1024)
-        if not packet:
-            break
-        data += packet
+	packet,_ = client_socket.recvfrom(BUFF_SIZE)
+	data = base64.b64decode(packet,' /')
+	npdata = np.fromstring(data,dtype=np.uint8)
+	frame = cv2.imdecode(npdata,1)
+	frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+	cv2.imshow("RECEIVING VIDEO",frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord('q'):
+		client_socket.close()
+		break
+	if cnt == frames_to_count:
+		try:
+			fps = round(frames_to_count/(time.time()-st))
+			st=time.time()
+			cnt=0
+		except:
+			pass
+	cnt+=1
 
-    packedMessageSize = data[:payloadSize]
-    data = data[payloadSize:]
-    messageSize = struct.unpack("Q", packedMessageSize)[0]
 
-    while len(data) < messageSize:
-        packet, _ = clientSocket.recvfrom(4 * 1024)
-        if not packet:
-            break
-        data += packet
 
-    frameData = data[:messageSize]
-    data = data[messageSize:]
-    frame = pickle.loads(frameData)
-    cv2.imshow("RECEIVING VIDEO", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-
-clientSocket.close()
