06840a47bcaed7c99decce23278e3fb57733041d
prolvalone <christopher02michael@gmail.com>
2024-06-27 15:56:18 -0400

    made server and client have camelcase

--- src/commTesting/client.py
+++ src/commTesting/client.py
@@ -5,31 +5,31 @@ import numpy as np
 import time
 import base64
 
-BUFF_SIZE = 65536
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-host_name = socket.gethostname()
-host_ip = '172.16.224.229'#  socket.gethostbyname(host_name)
-print(host_ip)
+bufferSize = 65536
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
+hostName = socket.gethostname()
+hostIp = '10.255.0.137'#  socket.gethostbyname(host_name)
+print(hostIp)
 port = 9999
 message = b'Hello'
 
-client_socket.sendto(message,(host_ip,port))
-fps,st,frames_to_count,cnt = (0,0,20,0)
+clientSocket.sendto(message, (hostIp,port))
+fps, st, framesToCount, cnt = (0,0,20,0)
 while True:
-	packet,_ = client_socket.recvfrom(BUFF_SIZE)
-	data = base64.b64decode(packet,' /')
-	npdata = np.fromstring(data,dtype=np.uint8)
-	frame = cv2.imdecode(npdata,1)
-	frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-	cv2.imshow("RECEIVING VIDEO",frame)
+	packet,_ = clientSocket.recvfrom(bufferSize)
+	data = base64.b64decode(packet, ' /')
+	npdata = np.fromstring(data, dtype=np.uint8)
+	frame = cv2.imdecode(npdata, 1)
+	frame = cv2.putText(frame, 'FPS: '+str(fps), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+	cv2.imshow("RECEIVING VIDEO", frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord('q'):
-		client_socket.close()
+		clientSocket.close()
 		break
-	if cnt == frames_to_count:
+	if cnt == framesToCount:
 		try:
-			fps = round(frames_to_count/(time.time()-st))
+			fps = round(framesToCount/(time.time()-st))
 			st=time.time()
 			cnt=0
 		except:
--- src/commTesting/server.py
+++ src/commTesting/server.py
@@ -5,39 +5,39 @@ import numpy as np
 import time
 import base64
 
-BUFF_SIZE = 65536
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-host_name = socket.gethostname()
-host_ip = '172.16.224.229'#  socket.gethostbyname(host_name)
-print(host_ip)
+bufferSize = 65536
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
+hostName = socket.gethostname()
+hostIp = '10.255.0.137'#  socket.gethostbyname(hostName)
+print(hostIp)
 port = 9999
-socket_address = (host_ip,port)
-server_socket.bind(socket_address)
-print('Listening at:',socket_address)
+socketAddress = (hostIp,port)
+serverSocket.bind(socketAddress)
+print('Listening at:', socketAddress)
 
 vid = cv2.VideoCapture(0) #  replace 'rocket.mp4' with 0 for webcam
-fps,st,frames_to_count,cnt = (0,0,20,0)
+fps, st, framesToCount, cnt = (0,0,20,0)
 
 while True:
-	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
-	print('GOT connection from ',client_addr)
+	msg,clientAddr = serverSocket.recvfrom(bufferSize)
+	print('GOT connection from ', clientAddr)
 	WIDTH=400
 	while(vid.isOpened()):
 		_,frame = vid.read()
 		frame = imutils.resize(frame,width=WIDTH)
-		encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+		encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY,80])
 		message = base64.b64encode(buffer)
-		server_socket.sendto(message,client_addr)
-		frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-		cv2.imshow('TRANSMITTING VIDEO',frame)
+		serverSocket.sendto(message,clientAddr)
+		frame = cv2.putText(frame, 'FPS: '+str(fps), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+		cv2.imshow('TRANSMITTING VIDEO', frame)
 		key = cv2.waitKey(1) & 0xFF
 		if key == ord('q'):
-			server_socket.close()
+			serverSocket.close()
 			break
-		if cnt == frames_to_count:
+		if cnt == framesToCount:
 			try:
-				fps = round(frames_to_count/(time.time()-st))
+				fps = round(framesToCount/(time.time()-st))
 				st=time.time()
 				cnt=0
 			except:
