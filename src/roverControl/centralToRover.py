b50b945184e2cb2accc2108413abf7d3c517f4ba
Zoe Rizzo <zjr1377@rit.edu>
2024-07-01 09:59:55 -0400

    contribution for the day

--- src/roverControl/centralToRover.py
+++ src/roverControl/centralToRover.py
@@ -1,19 +1,21 @@
 """
 Transmits rover controls either directly or through drone
 
-@author [name] [github]
+@author Zoe [@zizz-0]
 
-Date last modified: 06/26/2024
+Date last modified: 07/01/2024
 """
 
 import hid
 
+for device in hid.enumerate():
+    print(f"0x{device['vendor_id']:04x}:0x{device['product_id']:04x} {device['product_string']}")
+
 gamepad = hid.device()
-gamepad.open(0x1209, 0x4f54)
+gamepad.open(0x045e, 0x02ff) # controller vendor id, product id
 gamepad.set_nonblocking(True)
 
 while True:
     report = gamepad.read(64)
     if report:
         print(report)
-
