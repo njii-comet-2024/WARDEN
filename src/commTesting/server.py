7ca9a40753dac518558fd39ca7a3abb0036117dc
prolvalone <80829286+prolvalone@users.noreply.github.com>
2024-07-01 08:39:13 -0400

    Update server.py

--- src/commTesting/server.py
+++ src/commTesting/server.py
@@ -1,46 +1,46 @@
 
-# This is server code to send video and audio frames over UDP
+# This is server code to send video frames over UDP
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
 
-import socket
-import threading, wave, pyaudio, time
+bufferSize = 65536
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
+hostName = socket.gethostname()
+hostIp = '10.255.0.137'#  socket.gethostbyname(hostName)
+print(hostIp)
+port = 9999
+socketAddress = (hostIp,port)
+serverSocket.bind(socketAddress)
+print('Listening at:', socketAddress)
 
-host_name = socket.gethostname()
-host_ip = '10.255.0.137'#  socket.gethostbyname(host_name)
-print(host_ip)
-port = 9633
-# For details visit: www.pyshine.com
+vid = cv2.VideoCapture(0) #  replace 'rocket.mp4' with 0 for webcam
+fps, st, framesToCount, cnt = (0,0,20,0)
 
-def audio_stream_UDP():
-
-    BUFF_SIZE = 65536
-    server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
-
-    server_socket.bind((host_ip, (port)))
-    CHUNK = 10*1024
-    wf = wave.open("temp.wav")
-    p = pyaudio.PyAudio()
-    print('server listening at',(host_ip, (port)),wf.getframerate())
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    data = None
-    sample_rate = wf.getframerate()
-    while True:
-        msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
-        print('GOT connection from ',client_addr,msg)
-        
-        while True:
-            data = wf.readframes(CHUNK)
-            server_socket.sendto(data,client_addr)
-            time.sleep(0.8*CHUNK/sample_rate)
-            
-           
-                
-
-t1 = threading.Thread(target=audio_stream_UDP, args=())
-t1.start()
+while True:
+	msg,clientAddr = serverSocket.recvfrom(bufferSize)
+	print('GOT connection from ', clientAddr)
+	WIDTH=400
+	while(vid.isOpened()):
+		_,frame = vid.read()
+		frame = imutils.resize(frame,width=WIDTH)
+		encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY,80])
+		message = base64.b64encode(buffer)
+		serverSocket.sendto(message,clientAddr)
+		frame = cv2.putText(frame, 'FPS: '+str(fps), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+		cv2.imshow('TRANSMITTING VIDEO', frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord('q'):
+			serverSocket.close()
+			break
+		if cnt == framesToCount:
+			try:
+				fps = round(framesToCount/(time.time()-st))
+				st=time.time()
+				cnt=0
+			except:
+				pass
+		cnt+=1
 
