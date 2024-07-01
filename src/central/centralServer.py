bf72e29e4f2e35fb0339b2661d9a553606ae899b
Vito <gt6@njit.edu>
2024-07-01 14:23:51 -0400

    pretty much discord

--- src/central/centralServer.py
+++ src/central/centralServer.py
@@ -9,32 +9,37 @@ Date last modified: 07/1/2024
 import socket
 import sys
 
-#create a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def serverProgram():
+    #create a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#reserved a port on computer, can be anything
-port = 56789
+    #reserved a port on computer, can be anything
+    port = 56789
 
-#bind to the port, no ip in ip field which makes server listen to request
-s.bind(('', port))
-print ("socket binded to %s" %(port))
+    #bind to the port, no ip in ip field which makes server listen to request
+    s.bind(('', port))
+    print ("socket binded to %s" %(port))
 
 #put socket into listening mode
-s.listen(5)
-print ("socket is listening")
+    s.listen(5)
+    print ("socket is listening")
 
-while True:
-    #establish connection with client
-    c, addr = s.accept()
-    print ('Got connection from', addr)
+    while True:
+        #establish connection with client
+        c, addr = s.accept()
+        print ('Got connection from', addr)
 
-    #send message to the client. 
-    c.send('connected successfully'.encode())
+        #send message to the client. 
+        data = c.recv(1024).decode()
+        #if data is not recieved then break
+        if not data:
+            break
+
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        c.send(data.encode())
 
     #Close the connection
     c.close()
 
-    break
-
-#send commands to the drone to send to the ground vehicle
-#def commands_to_drone(data):
+serverProgram()
