61ccef56d05db4b021c75c7cf07ca2051e1c8ef9
prolvalone <80829286+prolvalone@users.noreply.github.com>
2024-07-02 14:55:15 -0400

    started CV capture

--- src/central/roverFeed.py
+++ src/central/roverFeed.py
@@ -1,14 +1,22 @@
 """
 Receives video feed from rover
 
-@author [name] [github]
+@author [Christopher Prol] [prolvalone]
 
-Date last modified: 06/26/2024
+Date last modified: 07/02/2024
 """
 
 # Libraries
+import cv2 as cv
+import pickle
+import struct
 
-class Control:
+
+class Camera:
 
     def __init__(self):
         print("initializing")
+
+    def getDroneFeed():
+        capture = cv.VideoCapture(2) # need origin of camera, 2 potentially works, potentially doesnt
+        
