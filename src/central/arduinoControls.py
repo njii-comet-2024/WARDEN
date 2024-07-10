8aa77ce66e5874fa44a5f50d95812c1c76db91f3
Zoe Rizzo <93719522+zizz-0@users.noreply.github.com>
2024-07-10 16:23:41 -0400

    Update arduinoControls.py

--- src/central/arduinoControls.py
+++ src/central/arduinoControls.py
@@ -13,35 +13,45 @@ print(joystick.get_name())
 joystick.init()
 
 joyControls = {
-    "up" : 11,
-    "down" : 12
+    "x" : 0,
+    "circle" : 1,
+    "triangle" : 2,
+    "square" : 3,
+    "leftBumper" : 4,
+    "rightBumper" : 5,
+    "leftTrigger" : 6,
+    "rightTrigger" : 7,
+    "leftCircle" : 8,
+    "rightCircle" : 9,
+    "leftJoyClick" : 12,
+    "rightJoyClick" : 13
 }
 
-controls = {
-    "upArrow" : 0,
-    "downArrow" : 0
-}
- 
+analogKeys = {0:0, 1:0, 2:0, 3:0, 4:-1, 5:-1}
+
+int inputControls[11]
+"""
+0  => left joystick analog  => left treads
+1  => right stick analog    => right treads
+2  => left trigger          => left whegs fwd
+3  => right trigger         => right whegs fwd
+4  => left bumper           => left whegs back
+5  => right bumper          => right whegs back
+6  => x                     => camera tilt / telescope down
+7  => circle                => camera swivel right
+8  => triangle              => camera tilt / telescope up
+9  => square                => camera swivel left
+10 => left circle           => camera type toggle 
+11 => right circle          => camera control toggle
+"""
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.JOYBUTTONDOWN:
             print(event)
-            if event.button == 11:
-                controls["upArrow"] = 1
-                print("UP")
-            if event.button == 12:
-                controls["downArrow"] = 1
-                print("DOWN")
-                
-        if event.type == pygame.JOYBUTTONUP:
-            if event.button == 11:
-                controls["upArrow"] = 0
-            if event.button == 12:
-                controls["downArrow"] = 0
-        
         if event.type == pygame.JOYAXISMOTION:
             print(event)
+            
                 
-    # encoded = pickle.dumps(controls)
         
     # ser.write(encoded) # Sending controls as a dictionary
