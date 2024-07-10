db75cc7b95b2066967820d26b1172613cc1dbdc5
Zoe Rizzo <93719522+zizz-0@users.noreply.github.com>
2024-07-10 15:29:01 -0400

    Create arduinoControls.ino

---
+++ src/central/arduinoControls.ino
@@ -0,0 +1,44 @@
+import serial
+import time
+import pygame
+import pickle
+ 
+# Adjust the port as needed
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+pygame.init()
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+print(joystick.get_name())
+joystick.init()
+
+joyControls = {
+    "up" : 11,
+    "down" : 12
+}
+
+controls = {
+    "upArrow" : 0,
+    "downArrow" : 0
+}
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            print(event)
+            if event.button == 11:
+                controls["upArrow"] = 1
+                print("UP")
+            if event.button == 12:
+                controls["downArrow"] = 1
+                print("DOWN")
+                
+        if event.type == pygame.JOYBUTTONUP:
+            if event.button == 11:
+                controls["upArrow"] = 0
+            if event.button == 12:
+                controls["downArrow"] = 0
+                
+    # encoded = pickle.dumps(controls)
+        
+    # ser.write(encoded) # Sending controls as a dictionary
