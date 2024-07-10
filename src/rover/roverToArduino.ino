4998139f4384c37a037a40d3156c3101bd516990
Zoe Rizzo <zjr1377@rit.edu>
2024-07-10 14:23:03 -0400

    added transmitter code

--- src/rover/roverToArduino.ino
+++ src/rover/roverToArduino.ino
@@ -1,7 +1,16 @@
-//map<string, int> controls;
+// map<string, int> controls;
 
 //controls["upArrow"] = 0;
 //controls["downArrow"] = 0;
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+const byte address[5] = {'R','x','a','A','z'};
+
+byte recv[2];
 
 int ENA = 5;
 int IN1 = 2;
@@ -10,22 +19,35 @@ int IN3 = 4;
 int IN4 = 7;
 int ENB = 6;
 
-void setup() {
+void setup(){
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENB, OUTPUT);
 
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENB, OUTPUT);
+    Serial.begin(9600);
+      
+      bool begin = radio.begin();
 
-  Serial.begin(9600);
+      if(begin){
+          Serial.println("Connected");
+      }
+      else{
+          Serial.println("NOT CONNECTED");
+      }
+      
+      radio.openWritingPipe(address);
+      radio.setPALevel(RF24_PA_MAX);
+      radio.stopListening(); // set as transmitter
 }
 
-void loop() {
-  if(serial.available() > 0){
-    controls = serial.read();
-    serial.write(controls)
-    sleep(1000)
-  }
+void loop(){
+    if(Serial.available() > 0){
+        recv = Serial.read();
+        radio.write(&recv, sizeof(recv));
+    }
+    
+    delay(500)
 }
