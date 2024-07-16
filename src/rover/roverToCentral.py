63273dcc428da03acbc9fa5b3fd9e11c0fce00c0
Zoe Rizzo <zjr1377@rit.edu>
2024-07-16 09:53:05 -0400

    NO MORE ARDUINO git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .! goodbye

--- src/rover/roverToCentral.py
+++ src/rover/roverToCentral.py
@@ -15,22 +15,111 @@ import socket
 import base64
 import time
 import serial
+from gpiozero import Motor
+from gpiozero import Servo
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
+
+# PINS
+# IN1 => CLOCKWISE
+# IN2 => COUNTER-CLOCKWISE
+# Motor 1 -- Right main treads
+M1_ENA = 0
+M1_IN1 = 0
+M1_IN2 = 0
+
+# Motor 2 -- Left main treads
+M2_ENA = 0
+M2_IN1 = 0
+M2_IN2 = 0
+
+# Motor 3 -- Left wheg treads
+M3_ENA = 0
+M3_IN1 = 0
+M3_IN2 = 0
+
+# Motor 4 -- Left wheg treads
+M4_ENA = 0
+M4_IN1 = 0
+M4_IN2 = 0
+
+# Motor 5 -- Right wheg treads (DM456AI)
+M5_ENA = 0
+M5_OPTO = 0
+M5_DIR = 0
+
+# Motor 6 -- Left wheg treads  (DM456AI)
+M6_ENA = 0
+M6_OPTO = 0
+M6_DIR = 0
+
+# Motor 7 -- Camera Telescope Linear Actuator
+M7_IN1 = 0
+M7_IN2 = 0
+
+stepsPerRevolution = 200; # for steppers == adjust based on steppers
+
+# Servo 1 -- Camera tilt
+S1_PIN = 0
+
+# Servo 2 -- Camera swivel
+S2_PIN = 0
+
+# Servo 3 -- Camera zoom
+S3_PIN = 0
+
+# Tread motors
+rightMainTread = Motor(M1_IN1, M1_IN2, M1_ENA)
+leftMainTread = Motor(M2_IN1, M2_IN2, M2_ENA)
+rightWhegTread = Motor(M3_IN1, M3_IN2, M3_ENA)
+leftWhegTread = Motor(M4_IN1, M4_IN2, M4_ENA)
+
+# Camera motor/servos
+telescope = Motor(M7_IN1, M7_IN2)
+tilt = Servo(S1_PIN)
+swivel = Servo(S2_PIN)
+zoom = Servo(S3_PIN)
 
 # Global variables
 IP = '192.168.110.19'  # change to rover IP
 PORT = 55555
 
-try:
-    arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    time.sleep(5)
-    print("Serial connection established")
-except serial.SerialException as e:
-    print(f"Error opening serial port {e}")
-    exit()
+cameraType = 0
 
+rightSpeed = 0
+leftSpeed = 0
+
+tiltPos = 0
+swivelPos = 0
+zoomPos = 0
+telePos = 0 # change to middle position
+
+"""
+State interface used to determine and switch camera state (from regular to IR)
+"""
+class CameraTypeState:
+    _state = 'REG'
+
+    """
+    Switches state from reg to IR (and vice versa)
+    """
+    def switch(self):
+        # TODO: add code to switch on microcontroller
+        if self._state == 'REG':
+            self._state = 'IR'
+            print("Camera IR")
+        elif self._state == 'IR':
+            self._state = 'REG'
+            print("Camera REG")
+        else:
+            pass # incorrect input
+    
+    """
+    Returns the current state
+    """
+    def getState(self):
+        return self._state
 
 """
 Class for manipulation Rover Camera
@@ -55,11 +144,10 @@ class Camera:
         print('Listening at:', socketAddress)
 
         vid = cv.VideoCapture(0)
-        cameraPos = bytearray(4)
 
         while True:
-            if arduino.in_waiting:
-                arduino.readinto(cameraPos)  # bytearray
+            cameraPos = [tiltPos, swivelPos, telePos, zoomPos]
+            cameraPosByte = bytearray(cameraPos)
 
             msg, clientAddr = serverSocket.recvfrom(bufferSize)
             print('GOT connection from ', clientAddr)
@@ -71,7 +159,7 @@ class Camera:
                 encoded, buffer = cv.imencode('.jpg', frame, [cv.IMWRITE_JPEG_QUALITY, 80])
 
                 message = base64.b64encode(buffer)
-                combined = cameraPos + message
+                combined = cameraPosByte + message
                 serverSocket.sendto(combined, clientAddr)
                 
                 cv.imshow('TRANSMITTING VIDEO', frame)
@@ -100,11 +188,10 @@ class Camera:
         videoConfig = picam2.create_video_configuration(main={"size": (640, 480)})
         picam2.configure(videoConfig)
         picam2.start()
-        cameraPos = bytearray(4)
 
         while True:
-            if arduino.in_waiting:
-                arduino.readinto(cameraPos)  # bytearray
+            cameraPos = [tiltPos, swivelPos, telePos, zoomPos]
+            cameraPosByte = bytearray(cameraPos)
 
             msg, clientAddr = serverSocket.recvfrom(bufferSize)
             print('GOT connection from ', clientAddr)
@@ -118,7 +205,7 @@ class Camera:
                 encoded, buffer = cv.imencode('.jpg', frame, [cv.IMWRITE_JPEG_QUALITY, 80])
 
                 message = base64.b64encode(buffer)
-                combined = cameraPos + message
+                combined = cameraPosByte + message
                 serverSocket.sendto(combined, clientAddr)
                 
                 cv.imshow('TRANSMITTING VIDEO', frame)
@@ -131,8 +218,6 @@ class Camera:
                     print('Server stopped by user')
                     exit(0)
                 
-
-
 """
 Class that defines a rover and its functionality
 """
@@ -144,6 +229,7 @@ class Rover:
         self.loopCount = 0
         self.control = 0  # control state -- even => central, odd => drone
         self.on = True  # Rover running
+        self.cameraType = CameraTypeState()
 
         # UDP
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -170,16 +256,101 @@ class Rover:
     """
     def drive(self):
         while self.on:
+            ctrls = []
             serializedControls, addr = self.sock.recvfrom(1024)
             controls = pickle.loads(serializedControls)  # unserializes controls
-            inputCtrls = ",".join(f"{key}:{value}" for key, value in controls.items())  # turns serialized controls from dict to string
-            # print(inputCtrls)
-            arduino.write((inputCtrls + '\n').encode())
-            print("Sent to Arduino: ", inputCtrls)
 
-            if arduino.in_waiting > 0:
-                line = arduino.readline().decode('utf-8').rstrip()
-                print(line)
+            leftSpeed = abs(controls["leftTread"])
+            rightSpeed = abs(controls["rightTread"])
+
+            if(controls["leftTreads"] > 0):
+                leftMainTread.forward(leftSpeed)
+                leftWhegTread.forward(leftSpeed)
+                ctrls.append("Left fwd")
+
+            if(controls["rightTreads"] < 0):
+                leftMainTread.backward(leftSpeed)
+                leftWhegTread.backward(leftSpeed)
+                ctrls.append("Left back")
+
+            if(controls["rightTreads"] > 0):
+                rightMainTread.forward(rightSpeed)
+                rightWhegTread.forward(rightSpeed)
+                ctrls.append("Right fwd")
+
+            if(controls["rightTreads"] < 0):
+                rightMainTread.backward(rightSpeed)
+                rightWhegTread.backward(rightSpeed)
+                ctrls.append("Right back")
+
+            if(controls["leftWheg"] > 0):
+                ctrls.append("Left wheg up")
+
+            if(controls["leftWheg"] < 0):
+                ctrls.append("Left wheg down")
+            
+            if(controls["rightWheg"] > 0):
+                ctrls.append("Right wheg up")
+
+            if(controls["rightWheg"] < 0):
+                ctrls.append("Right wheg down")
+
+            if(controls["cameraTypeToggle"] > 0):
+                self.cameraType.switch()
+                ctrls.append("Camera switch")
+
+            if(controls["cameraTelescope"] > 0):
+                # find max
+                # if telePos < max
+                telescope.forward()
+                telePos += 1
+                ctrls.append("Telescope up")
+            
+            if(controls["cameraTelescope"] < 0):
+                if(telePos > 0):
+                    telescope.backward()
+                    telePos -= 1
+                    ctrls.append("Telescope down")
+
+            if(controls["cameraTilt"] > 0):
+                if(tiltPos < 1):
+                    tiltPos += 0.05
+                    tilt.value = tiltPos
+                    ctrls.append("Tilt up")
+            
+            if(controls["cameraTilt"] < 0):
+                if(tiltPos > -1):
+                    tiltPos -= 0.05
+                    tilt.value = tiltPos
+                    ctrls.append("Tilt down")
+
+            if(controls["cameraLeft"] > 0):
+                if(swivelPos > -1):
+                    swivelPos -= 0.05
+                    swivel.value = swivelPos
+                    ctrls.append("Swivel left")
+            
+            if(controls["cameraRight"] > 0):
+                if(swivelPos < 1):
+                    swivelPos += 0.05
+                    swivel.value = swivelPos
+                    ctrls.append("Swivel right")
+            
+            if(controls["cameraZoom"] > 0):
+                if(zoomPos < 1):
+                    zoomPos += 0.5
+                    zoom.value = zoomPos
+                    ctrls.append("Zoom in")
+
+            if(controls["cameraZoom"] < 0):
+                if(zoomPos > -1):
+                    zoomPos -= 0.5
+                    zoom.value = zoomPos
+                    ctrls.append("Zoom out")
+            
+            if(ctrls):
+                print(ctrls)
+            
 
 
 rover = Rover()
