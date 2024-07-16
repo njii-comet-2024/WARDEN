2bce8bca78f45d68cc7b4aff47a547ec202ccad3
Zoe Rizzo <zjr1377@rit.edu>
2024-07-16 13:17:58 -0400

    this will probably not work !

--- src/rover/roverToCentral.py
+++ src/rover/roverToCentral.py
@@ -82,7 +82,9 @@ swivel = Servo(S2_PIN)
 zoom = Servo(S3_PIN)
 
 # Global variables
-IP = '192.168.110.19'  # change to rover IP
+ROVER_IP = '192.168.110.19'  # change to rover IP
+CENTRAL_IP = '192.168.110.19' # change to central IP
+
 PORT = 55555
 
 cameraType = 0
@@ -122,7 +124,236 @@ class CameraTypeState:
         return self._state
 
 """
+Class that defines a rover and its functionality
+Receives drive controls, runs them on rover, and transmits video feed and camera positions back to central
+"""
+class Rover:
+    def __init__(self):
+        print("Initializing...")
+        self.loopCount = 0
+        self.control = 0  # control state -- even => central, odd => drone
+        self.on = True  # Rover running
+        self.cameraType = CameraTypeState()
+
+        # UDP
+        self.bufferSize = 65536
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.bufferSize)
+        self.sock.bind((ROVER_IP, PORT))
+
+        # Initialize Picamera2
+        self.picam2 = Picamera2()
+        videoConfig = self.picam2.create_video_configuration(main={"size": (640, 480)})
+        self.picam2.configure(videoConfig)
+        self.picam2.start()
+    
+    """
+    Maps a number from one range to another
+    @param `num` : number to re-map
+    @param `inMin` : original range min
+    @param `inMax` : original range max
+    @param `outMin` : target range min
+    @param `outMax` : target range max
+    """
+    def numToRange(num, inMin, inMax, outMin, outMax):
+        return outMin + (float(num - inMin) / float(inMax - inMin) * (outMax
+                        - outMin))
+    
+    """
+    Transmits Rover Video Data from a usb camera Over UDP sockets, acting as the server
+    FOR TESTING ONLY
+    """
+    def transmitUSBCamFeed(self):
+        bufferSize = 65536
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
+        hostName = socket.gethostname()
+        hostIp = socket.gethostbyname(hostName)
+        print(hostIp)
+        port = 9999
+        socketAddress = (hostIp, port)
+        serverSocket.bind(socketAddress)
+        print('Listening at:', socketAddress)
+
+        vid = cv.VideoCapture(0)
+
+        while True:
+            # re-maps camera values and converts to bytearray
+            newTilt = self.numToRange(tiltPos, -1, 1, 0, 180)
+            newSwivel = self.numToRange(swivelPos, -1, 1, 0, 205)
+            newTele = self.numToRange(telePos, 0, 1, 0, 180) # figure out actual max
+            newZoom = self.numToRange(zoomPos, -1, 1, 0, 10)
+            cameraPos = [newTilt, newSwivel, newTele, newZoom]
+            cameraPosByte = bytearray(cameraPos)
+
+            msg, clientAddr = serverSocket.recvfrom(bufferSize)
+            print('GOT connection from ', clientAddr)
+            WIDTH = 400
+            HEIGHT = 1080
+            while vid.isOpened():
+                _, frame = vid.read()
+                frame = cv.resize(frame, (WIDTH, HEIGHT))
+                encoded, buffer = cv.imencode('.jpg', frame, [cv.IMWRITE_JPEG_QUALITY, 80])
+
+                message = base64.b64encode(buffer)
+                combined = cameraPosByte + message
+                serverSocket.sendto(combined, clientAddr)
+                
+                cv.imshow('TRANSMITTING VIDEO', frame)
+                key = cv.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    serverSocket.close()
+                    break
+
+    """
+    Starts the rover and runs the transmission and drive loops
+    THERE IS A CHANCE THIS DOES NOT WORK -- TEST TO SEE IF SEPARATING INTO HELPER CLASSES CAUSES LAG
+    """
+    def start(self):
+        print("Rover starting...")
+        while self.on:
+            self.transmitFeed()
+            self.drive()
+
+    """
+    Transmits camera feed from Picamera2 to Central via UDP sockets
+    """
+    def transmitFeed(self):
+        # re-maps camera values and converts to bytearray
+            newTilt = self.numToRange(tiltPos, -1, 1, 0, 180)
+            newSwivel = self.numToRange(swivelPos, -1, 1, 0, 205)
+            newTele = self.numToRange(telePos, 0, 1, 0, 180) # figure out actual max
+            newZoom = self.numToRange(zoomPos, -1, 1, 0, 10)
+            cameraPos = [newTilt, newSwivel, newTele, newZoom]
+            cameraPosByte = bytearray(cameraPos)
+
+            msg, clientAddr = self.sock.recvfrom(self.bufferSize)
+            print('GOT connection from ', clientAddr)
+            WIDTH = 400
+            while True:
+                buffer = self.picam2.capture_array("main")
+                frame = cv.cvtColor(buffer, cv.COLOR_RGB2BGR)
+                frame = cv.resize(frame, (WIDTH, int(WIDTH * frame.shape[1] / frame.shape[0])), interpolation=cv.INTER_AREA)
+                print('Resized frame size:', frame.shape)
+
+                encoded, buffer = cv.imencode('.jpg', frame, [cv.IMWRITE_JPEG_QUALITY, 80])
+
+                message = base64.b64encode(buffer)
+                combined = cameraPosByte + message
+                self.sock.sendto(combined, clientAddr)
+                
+                cv.imshow('TRANSMITTING VIDEO', frame)
+                key = cv.waitKey(1) & 0xFF
+                
+                if key == ord('q'):
+                    self.sock.close()
+                    self.picam2.stop()
+                    cv.destroyAllWindows()
+                    print('Server stopped by user')
+                    exit(0)
+    """
+    Main drive loop
+    Will receive controller input from central and run controls on rover
+    """
+    def drive(self):
+        ctrls = []
+        serializedControls, addr = self.sock.recvfrom(1024)
+        controls = pickle.loads(serializedControls)  # unserializes controls
+
+        leftSpeed = abs(controls["leftTread"])
+        rightSpeed = abs(controls["rightTread"])
+
+        if(controls["leftTreads"] > 0):
+            leftMainTread.forward(leftSpeed)
+            leftWhegTread.forward(leftSpeed)
+            ctrls.append("Left fwd")
+
+        if(controls["rightTreads"] < 0):
+            leftMainTread.backward(leftSpeed)
+            leftWhegTread.backward(leftSpeed)
+            ctrls.append("Left back")
+
+        if(controls["rightTreads"] > 0):
+            rightMainTread.forward(rightSpeed)
+            rightWhegTread.forward(rightSpeed)
+            ctrls.append("Right fwd")
+
+        if(controls["rightTreads"] < 0):
+            rightMainTread.backward(rightSpeed)
+            rightWhegTread.backward(rightSpeed)
+            ctrls.append("Right back")
+
+        if(controls["leftWheg"] > 0):
+            ctrls.append("Left wheg up")
+
+        if(controls["leftWheg"] < 0):
+            ctrls.append("Left wheg down")
+        
+        if(controls["rightWheg"] > 0):
+            ctrls.append("Right wheg up")
+
+        if(controls["rightWheg"] < 0):
+            ctrls.append("Right wheg down")
+
+        if(controls["cameraTypeToggle"] > 0):
+            self.cameraType.switch()
+            ctrls.append("Camera switch")
+
+        if(controls["cameraTelescope"] > 0):
+            # find max
+            # if telePos < max
+            telescope.forward()
+            telePos += 1
+            ctrls.append("Telescope up")
+        
+        if(controls["cameraTelescope"] < 0):
+            if(telePos > 0):
+                telescope.backward()
+                telePos -= 1
+                ctrls.append("Telescope down")
+
+        if(controls["cameraTilt"] > 0):
+            if(tiltPos < 1):
+                tiltPos += 0.05
+                tilt.value = tiltPos
+                ctrls.append("Tilt up")
+        
+        if(controls["cameraTilt"] < 0):
+            if(tiltPos > -1):
+                tiltPos -= 0.05
+                tilt.value = tiltPos
+                ctrls.append("Tilt down")
+
+        if(controls["cameraLeft"] > 0):
+            if(swivelPos > -1):
+                swivelPos -= 0.05
+                swivel.value = swivelPos
+                ctrls.append("Swivel left")
+        
+        if(controls["cameraRight"] > 0):
+            if(swivelPos < 1):
+                swivelPos += 0.05
+                swivel.value = swivelPos
+                ctrls.append("Swivel right")
+        
+        if(controls["cameraZoom"] > 0):
+            if(zoomPos < 1):
+                zoomPos += 0.5
+                zoom.value = zoomPos
+                ctrls.append("Zoom in")
+
+        if(controls["cameraZoom"] < 0):
+            if(zoomPos > -1):
+                zoomPos -= 0.5
+                zoom.value = zoomPos
+                ctrls.append("Zoom out")
+        
+        if(ctrls):
+            print(ctrls)
+
+"""
 Class for manipulation Rover Camera
+NO LONGER USED -- SAVE IN CASE
 """
 class Camera:
     def __init__(self):
@@ -242,8 +473,9 @@ class Camera:
                 
 """
 Class that defines a rover and its functionality
+NO LONGER USED -- SAVE IN CASE
 """
-class Rover:
+class Receiver:
     """
     Initializes an instance of Rover 
     """
@@ -274,7 +506,7 @@ class Rover:
 
     """
     Main drive loop
-    Will receive controller input from central and transmit to arduino
+    Will receive controller input from central and run controls on rover
     """
     def drive(self):
         while self.on:
@@ -374,9 +606,5 @@ class Rover:
                 print(ctrls)
             
 
-
 rover = Rover()
 rover.start()
-
-# cam = Camera
-# cam.transmitUSBCamFeed()
