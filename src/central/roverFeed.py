0dda12cc0128c98de09b620a3e5a35322be6d355
Vito <gt6@njit.edu>
2024-07-31 14:02:28 -0400

    merge conflict

--- src/central/roverFeed.py
+++ src/central/roverFeed.py
@@ -14,7 +14,11 @@ import numpy as np
 import base64
 import cvzone
 
+<<<<<<< HEAD
 ROVER_IP = '172.168.10.116'
+=======
+ROVER_IP = '192.168.110.78'
+>>>>>>> 8bdc810 (hello)
 TOP_HORIZ = -70
 TOP_VERT = 0
 
@@ -32,22 +36,23 @@ class videoReciever:
     """
     This function recieves Rover Cam footage from the PI Camera.  
     """
-    def receiveRoverCam(roverIP):
+    def receiveRoverCam(ROVER_IP):
         #initiate Window and resize
         cv.namedWindow('TESTING HUD', cv.WINDOW_NORMAL)
         cv.resizeWindow('TESTING HUD', 1024, 600)
         #socket information
         bufferSize = 65536
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
-        print(roverIP)
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufferSize)
+        print(ROVER_IP)
         port = 9999                                 # can change based on possible interference, etc
         message = b'Hello'                          # test message
        
         #connect to server socket
-        clientSocket.sendto(message, (roverIP,port))
+        serverSocket.sendto(message, (ROVER_IP,port))
 
         #read the image files
+<<<<<<< HEAD
         #may have to upload images to the actual Pi in order to work  - - relative path may not work due to not having repo cloned
         hudTop = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/hudCompassHorizontal.png', cv.IMREAD_UNCHANGED)
         hudSide = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/hudCompassHorizontal.png', cv.IMREAD_UNCHANGED)
@@ -55,23 +60,32 @@ class videoReciever:
         hudSideIndicator = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/arrow.png', cv.IMREAD_UNCHANGED)
         hudHeightIndicator = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/arrowRed.png', cv.IMREAD_UNCHANGED)
         infoBackground = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/blackRectangle.png', cv.IMREAD_UNCHANGED)
+=======
+        #src/assets
+        # hudTop = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/hudCompassHorizontal.png', cv.IMREAD_UNCHANGED)
+        # hudSide = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/hudCompassHorizontal.png', cv.IMREAD_UNCHANGED)
+        # hudTopIndicator = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/arrow.png', cv.IMREAD_UNCHANGED)
+        # hudSideIndicator = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/arrow.png', cv.IMREAD_UNCHANGED)
+        # hudHeightIndicator = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/arrowRed.png', cv.IMREAD_UNCHANGED)
+        # infoBackground = cv.imread('/Users/chris/OneDrive/Desktop/WARDEN/src/assets/blackRectangle.png', cv.IMREAD_UNCHANGED)
+>>>>>>> 8bdc810 (hello)
 
-        #rotate and resize images to be properly aligned
-        hudTop = cv.rotate(hudTop, cv.ROTATE_180)
-        hudTop = cv.resize(hudTop, (0, 0), None, 1.5, 1)
-        hudSide = cv.rotate(hudSide, cv.ROTATE_90_CLOCKWISE)
-        hudSide = cv.resize(hudSide, (0, 0), None, 1.5, 1)
-        hudSideIndicator = cv.rotate(hudSideIndicator, cv.ROTATE_90_COUNTERCLOCKWISE)
-        hudSideIndicator = cv.resize(hudSideIndicator, (0,0), None, .1, .1)
-        hudTopIndicator = cv.resize(hudTopIndicator, (0, 0), None, .1, .1)
-        hudHeightIndicator = cv.resize(hudHeightIndicator, (0, 0), None, .4, .1)
-        infoBackground = cv.resize(infoBackground, (0, 0), None, 1, .6)
-        infoBackground = cv.rotate(infoBackground, cv.ROTATE_90_CLOCKWISE)
+        # #rotate and resize images to be properly aligned
+        # hudTop = cv.rotate(hudTop, cv.ROTATE_180)
+        # hudTop = cv.resize(hudTop, (0, 0), None, 1.5, 1)
+        # hudSide = cv.rotate(hudSide, cv.ROTATE_90_CLOCKWISE)
+        # hudSide = cv.resize(hudSide, (0, 0), None, 1.5, 1)
+        # hudSideIndicator = cv.rotate(hudSideIndicator, cv.ROTATE_90_COUNTERCLOCKWISE)
+        # hudSideIndicator = cv.resize(hudSideIndicator, (0,0), None, .1, .1)
+        # hudTopIndicator = cv.resize(hudTopIndicator, (0, 0), None, .1, .1)
+        # hudHeightIndicator = cv.resize(hudHeightIndicator, (0, 0), None, .4, .1)
+        # infoBackground = cv.resize(infoBackground, (0, 0), None, 1, .6)
+        # infoBackground = cv.rotate(infoBackground, cv.ROTATE_90_CLOCKWISE)
 
         #loop for displaying video
         while True:
             #recieve Packet
-            packet,_ = clientSocket.recvfrom(bufferSize)
+            packet,_ = serverSocket.recvfrom(bufferSize)
             #interpret packet
             cameraPosLength = 4
             cameraPos = packet[:cameraPosLength]
@@ -80,36 +94,36 @@ class videoReciever:
             data = base64.b64decode(imgData, ' /')
             npdata = np.fromstring(data, dtype=np.uint8)
             frame = cv.imdecode(npdata, 1)
-            #adds indicator bars to HUD
-            imgResult = cvzone.overlayPNG(frame, hudTop, [TOP_HORIZ, TOP_VERT]) # adds top Hud
-            imgResult = cvzone.overlayPNG(imgResult, hudSide, [SIDE_HORIZ, SIDE_VERT]) #adds side hud
-            imgResult = cvzone.overlayPNG(imgResult, infoBackground, [5, 350])# adds info background for ease of seeing words
+            # #adds indicator bars to HUD
+            # imgResult = cvzone.overlayPNG(frame, hudTop, [TOP_HORIZ, TOP_VERT]) # adds top Hud
+            # imgResult = cvzone.overlayPNG(imgResult, hudSide, [SIDE_HORIZ, SIDE_VERT]) #adds side hud
+            # imgResult = cvzone.overlayPNG(imgResult, infoBackground, [5, 350])# adds info background for ease of seeing words
             #create positional data variables
-            cameraPosData = [int(b) for b in cameraPos]
-            cameraHeight = cameraPosData[0]
-            cameraTilt = cameraPosData[1]
-            cameraRotation = cameraPosData[2]
-            cameraZoom = cameraPosData[3]
-            print(cameraPosData)
+            # cameraPosData = [int(b) for b in cameraPos]
+            # cameraHeight = cameraPosData[0]
+            # cameraTilt = cameraPosData[1]
+            # cameraRotation = cameraPosData[2]
+            # cameraZoom = cameraPosData[3]
+            # print(cameraPosData)
 
             #display location coords
-            imgResult = cv.putText(imgResult,'Height: ' + str(cameraHeight) + ' Zoom: ' + str(cameraZoom) + 'x', (40, 370), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0),2)
-            imgResult = cv.putText(imgResult, 'ValueY: ' + str(cameraTilt) + '  ValueX: ' + str(cameraRotation), (40, 390), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0),2)
+            #imgResult = cv.putText(imgResult,'Height: ' + str(cameraHeight) + ' Zoom: ' + str(cameraZoom) + 'x', (40, 370), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0),2)
+           #imgResult = cv.putText(imgResult, 'ValueY: ' + str(cameraTilt) + '  ValueX: ' + str(cameraRotation), (40, 390), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0),2)
             #display max limit messages
-            if(cameraTilt == 0 or cameraTilt == 180):
-                imgResult = cv.putText(imgResult, 'Y AXIS LIMIT REACHED' , (390, 380), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
-            if(cameraRotation == 0 or cameraRotation == 205):
-                imgResult = cv.putText(imgResult, 'X AXIS LIMIT REACHED' , (390, 350), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
-            if(cameraHeight == 0 or cameraHeight == 180):
-                imgResult = cv.putText(imgResult, 'HEIGHT LIMIT REACHED' , (650, 350), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
-            if(cameraZoom == 0 or cameraZoom == 10):
-                imgResult = cv.putText(imgResult, 'ZOOM LIMIT REACHED' , (650, 380), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
-            #overlay indicator
-            imgResult = cvzone.overlayPNG(imgResult, hudTopIndicator, [cameraRotation * 5, TOP_VERT])#adds moving vertical
-            imgResult = cvzone.overlayPNG(imgResult, hudSideIndicator, [SIDE_HORIZ, cameraTilt * 2])
-            imgResult = cvzone.overlayPNG(imgResult, hudHeightIndicator, [1030, cameraHeight * 2])
-            #display video
-            cv.imshow('TESTING HUD', frame)
+            # if(cameraTilt == 0 or cameraTilt == 180):
+            #     imgResult = cv.putText(imgResult, 'Y AXIS LIMIT REACHED' , (390, 380), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
+            # if(cameraRotation == 0 or cameraRotation == 205):
+            #     imgResult = cv.putText(imgResult, 'X AXIS LIMIT REACHED' , (390, 350), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
+            # if(cameraHeight == 0 or cameraHeight == 180):
+            #     imgResult = cv.putText(imgResult, 'HEIGHT LIMIT REACHED' , (650, 350), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
+            # if(cameraZoom == 0 or cameraZoom == 10):
+            #     imgResult = cv.putText(imgResult, 'ZOOM LIMIT REACHED' , (650, 380), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255),2)
+            # #overlay indicator
+            # imgResult = cvzone.overlayPNG(imgResult, hudTopIndicator, [cameraRotation * 5, TOP_VERT])#adds moving vertical
+            # imgResult = cvzone.overlayPNG(imgResult, hudSideIndicator, [SIDE_HORIZ, cameraTilt * 2])
+            # imgResult = cvzone.overlayPNG(imgResult, hudHeightIndicator, [1030, cameraHeight * 2])
+            # #display video
+            # cv.imshow('TESTING HUD', frame)
 
             #exit key
             if cv.waitKey(20) &0xFF == ord('q'):
---
+++ src/central/theRoverFeed.py
@@ -0,0 +1,59 @@
+'''
+recieves camera feed from the rover and displays it
+
+@author [Vito Tribuzio][Snoopy-0]
+
+Date last modified: 07/22/2024
+'''
+
+#libraries
+import cv2
+import socket
+import struct
+import pickle
+import numpy as np
+
+#socket parameters
+hostIP = '192.168.110.78'
+hostPort = 9999
+
+#initialize socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((hostIP, hostPort))
+serverSocket.listen(5)
+print("Listening for connections...")
+
+# Accept connection
+clientSocket, addr = serverSocket.accept()
+print(f"Connection from: {addr}")
+connection = clientSocket.makefile('rb')
+
+while True:
+    # Read message length
+    packed_msg_size = connection.read(struct.calcsize("Q"))
+    if not packed_msg_size:
+        break
+
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    # Read message data
+    data = b""
+    while len(data) < msg_size:
+        data += connection.read(msg_size - len(data))
+
+    # Deserialize frame
+    frame_data = pickle.loads(data)
+    frame = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Display frame
+    cv2.imshow("Receiving Video", frame_rgb)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+connection.close()
+clientSocket.close()
+serverSocket.close()
+cv2.destroyAllWindows()
