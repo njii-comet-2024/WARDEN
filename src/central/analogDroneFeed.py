85930e0ce0b1b078d5beb1c122ceb43d615e12e0
prolvalone <80829286+prolvalone@users.noreply.github.com>
2024-08-05 11:47:17 -0400

    alias

--- src/central/analogDroneFeed.py
+++ src/central/analogDroneFeed.py
@@ -1,12 +1,12 @@
-import cv2
+import cv2 as cv
 
-cap = cv2.VideoCapture(0)  # Replace 0 with the index of your webcam if necessary
+cap = cv.VideoCapture(0)  # Replace 0 with the index of your webcam if necessary
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('Analog', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv.imshow('Analog', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
