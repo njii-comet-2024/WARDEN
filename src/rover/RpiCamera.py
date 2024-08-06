cc5f2c8dc49bb101e74248b58239ccd6b0d72b01
soumya-khera <75648606+soumya-khera@users.noreply.github.com>
2024-08-06 17:24:45 -0400

    removed comma

--- src/rover/RpiCamera.py
+++ src/rover/RpiCamera.py
@@ -109,7 +109,7 @@ class Camera():
         
     def startPreview(self,width=1024,length=600):
         self.is_running = True
-        self.capture_ = threading.Thread(target=self.captureAndPreviewThread, args=(width,length,))
+        self.capture_ = threading.Thread(target=self.captureAndPreviewThread, args=(width,length))
         self.capture_.setDaemon(True)
         self.capture_.start()
 
